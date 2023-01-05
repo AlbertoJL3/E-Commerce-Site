58bc93ca711d95f2c7ae3c2063883eb791d6b1c7
Beto Lopez <363alberto@gmail.com>
2023-01-04 22:27:06 -0400

    finished category-route!

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,94 +2,77 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-router.get('/', (req, res) => {
+
+router.get('/', async (req, res) => {
   // find all categories
-  Category.findAll({
-    include: [
-      {
-        model: Product,
-        as: 'products'
-      }
-    ]
-  })
-    .then(dbCategoryData => res.json(dbCategoryData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+  // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product }]
     });
+
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  Category.findOne({
-    where: {
-      id: req.params.id
-    },
-    include: [
-      {
-        model: Product,
-        as: 'products'
-      }
-    ]
-  })
-    .then(dbCategoryData => {
-      if (!dbCategoryData) {
-        res.status(404).json({ message: 'No category found with this id' });
-        return;
-      }
-      res.json(dbCategoryData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+  // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
     });
+
+    if (!category) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
-  Category.create({
-    category_name: req.body.category_name
-  })
-    .then(dbCategoryData => res.json(dbCategoryData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // find the product by its id
-  Product.findByPk(req.params.id)
-    .then((product) => {
-      // update the product with the new data
-      return product.update(req.body);
-    })
-    .then((product) => {
-      // send the updated product back to the client
-      res.json(product);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(400).json(err);
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const category = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
     });
+    res.status(200).json(category)
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const category = await Category.destroy({
-      where: {
-        id: req.params.id
-      }
+      where: {id: req.params.id}
     });
     if (!category) {
-      res.status(404).json({ message: 'No category found with this id' });
+      res.status(404).json({message:  'No category with this id'});
       return;
     }
-    res.json(dbCategoryData);
-
-  } catch (err) {
-    console.log(err);
+    res.status(200).json(category);
+   } catch (err) {
     res.status(500).json(err);
-  }
+   }
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize.sync()
+  .then(() => {
+    // start the server
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}.`);
+    });
+  });
