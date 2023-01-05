a2aac121ec8d9aa191e3b2f0670beccadc7d3579
Beto Lopez <363alberto@gmail.com>
2023-01-04 20:57:00 -0400

    added ProductTag columns in models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,31 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: sequelize.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: sequelize.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      },
+    },
   },
   {
     sequelize,
